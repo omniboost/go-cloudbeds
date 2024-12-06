36d09b0b4b5ae9ec067e3dcf9db5fc6f4294299b
Chris van Valderen <chris@omniboost.io>
2024-12-06 16:52:55 +0100

    updated coms

--- get_reports_query_data.go
+++ get_reports_query_data.go
@@ -5,11 +5,6 @@ import (
 	"net/url"
 )
 
-// Payment - getTransactions
-// Get a list of transactions for a property, or list of properties, for the
-// date range specified. If no date range or reservation is specified, it will
-// return the transactions for the last 7 days, unless stated otherwise.
-
 func (c *Client) NewGetReportsQueryDataRequest() GetReportsQueryDataRequest {
 	return GetReportsQueryDataRequest{
 		client:      c,
@@ -35,66 +30,26 @@ func (c *Client) NewGetReportsQueryDataQueryParams() *GetReportsQueryDataQueryPa
 }
 
 type GetReportsQueryDataQueryParams struct {
-	// ID for the properties to be queried (comma-separated).
-	// It can be omitted if the API key is single-property, or to get results from
-	// all properties on an association.
-	PropertyID string `schema:"propertyID,omitempty"`
-	// If the response should include debit transactions
-	// Standaard waarde: true
-	// Standaard waarde: true
-	IncludeDebit bool `schema:"includeDebit,omitempty"`
-	// If the response should include credit transactions
-	IncludeCredit bool `schema:"includeCredit,omitempty"`
-	// If the response should include deleted transactions
-	// Standaard waarde: false
-	IncludeDeleted bool `schema:"includeDeleted,omitempty"`
-	// Reservation Unique Identifier, used to filter transactions result
-	// If reservationID is informed, and dates are not, all transactions with the
-	// reservationID will be returned.
-	ReservationID string `schema:"reservationID,omitempty"`
-	// Sub Reservation Identifier, used to filter transactions result
-	SubReservationID string `schema:"subReservationID,omitempty"`
-	// Room ID, used to filter transactions result
-	RoomID string `schema:"roomID,omitempty"`
-	// Guest ID, used to filter transactions result
-	GuestID int `schema:"guestID,omitempty"`
-	// House Account ID, used to filter transactions result
-	HouseAccountID int `schema:"houseAccountID,omitempty"`
-	// Inferior limit date, used to filter transactions result (posted transaction date)
-	ResultsFrom Date `schema:"resultsFrom,omitempty"`
-	// Superior limit date, used to filter transactions result (posted transaction date)
-	ResultsTo Date `schema:"resultsTo,omitempty"`
-	// Inferior limit date, used to filter transactions result
-	ModifiedFrom Date `schema:"modifiedFrom,omitempty"`
-	// Superior limit date, used to filter transactions result
-	ModifiedTo Date `schema:"modifiedTo,omitempty"`
-	// Inferior limit datetime, used to filter transactions result (creation date of the transaction).
-	// If informed, all other dates are ignored (except createdTo).
-	// If createdFrom is informed, but createdTo is not, the call will return all results since this datetime.
-	// Necessary only if createdTo is sent.
-	// If time portion not given, assumes 00:00:00.
-	CreatedFrom DateTime `schema:"createdFrom,omitempty"`
-	// Superior limit datetime, used to filter transactions result (creation date of the transaction).
-	// If informed (together with createdFrom), all other dates are ignored.
-	// If time portion not given, assumes 23:59:59.
-	CreatedTo DateTime `schema:"createdTo,omitempty"`
-	// transactionFilter optioneel	String
-	// Transaction filter is used to filter transactions result
-	// Standaard waarde: simple_transactions,adjustments,adjustments_voids,voids,refunds
+	PropertyID        string            `schema:"propertyID,omitempty"`
+	IncludeDebit      bool              `schema:"includeDebit,omitempty"`
+	IncludeCredit     bool              `schema:"includeCredit,omitempty"`
+	IncludeDeleted    bool              `schema:"includeDeleted,omitempty"`
+	ReservationID     string            `schema:"reservationID,omitempty"`
+	SubReservationID  string            `schema:"subReservationID,omitempty"`
+	RoomID            string            `schema:"roomID,omitempty"`
+	GuestID           int               `schema:"guestID,omitempty"`
+	HouseAccountID    int               `schema:"houseAccountID,omitempty"`
+	ResultsFrom       Date              `schema:"resultsFrom,omitempty"`
+	ResultsTo         Date              `schema:"resultsTo,omitempty"`
+	ModifiedFrom      Date              `schema:"modifiedFrom,omitempty"`
+	ModifiedTo        Date              `schema:"modifiedTo,omitempty"`
+	CreatedFrom       DateTime          `schema:"createdFrom,omitempty"`
+	CreatedTo         DateTime          `schema:"createdTo,omitempty"`
 	TransactionFilter TransactionFilter `schema:"transactionFilter,omitempty"`
-	// Results page number
-	// Standaard waarde: 1
-	PageNumber int `schema:"pageNumber,omitempty"`
-	// Results page size. Max = 100
-	// Standaard waarde: 100
-	PageSize int `schema:"pageSize,omitempty"`
-	// Sort response results by field
-	// Toegestane waarden: transactionDateTime, transactionModifiedDateTime, guestCheckIn, guestCheckOut
-	SortBy string `schema:"sortBy,omitempty"`
-	// Order response in DESCending or ASCending order, used together with sortBy
-	// Standaard waarde: desc
-	// Toegestane waarden: desc, asc
-	OrderBy string `schema:"orderBy,omitempty"`
+	PageNumber        int               `schema:"pageNumber,omitempty"`
+	PageSize          int               `schema:"pageSize,omitempty"`
+	SortBy            string            `schema:"sortBy,omitempty"`
+	OrderBy           string            `schema:"orderBy,omitempty"`
 }
 
 func (p GetReportsQueryDataQueryParams) ToURLValues() (url.Values, error) {
@@ -161,44 +116,38 @@ type GetReportsQueryDataResponseBody struct {
 	Total   Int    `json:"total"`
 	Message string `json:"message"`
 	Data    []struct {
-		PropertyID                     Int         `json:"propertyID"`                     // Property ID
-		ReservationID                  string      `json:"reservationID"`                  // Reservation ID
-		SubReservationID               string      `json:"subReservationID"`               // Sub Reservation ID
-		HouseAccountID                 interface{} `json:"houseAccountID"`                 // House Account ID
-		HouseAccountName               string      `json:"houseAccountName"`               // House Account Name
-		GuestID                        string      `json:"guestID"`                        // Guest ID
-		PropertyName                   string      `json:"propertyName"`                   // Property Name
-		TransactionDateTime            DateTime    `json:"transactionDateTime"`            // DateTime that the transaction was stored
-		TransactionDateTimeUTC         DateTime    `json:"transactionDateTimeUTC"`         // DateTime that the transaction was stored, in UTC timezone
-		TransactionModifiedDateTime    DateTime    `json:"transactionModifiedDateTime"`    // DateTime that the transaction was last modified
-		TransactionModifiedDateTimeUTC DateTime    `json:"transactionModifiedDateTimeUTC"` // DateTime that the transaction was slast modified, in UTC timezone
-		GuestCheckin                   Date        `json:"guestCheckin"`                   // Reservation Check-in date
-		GuestCheckout                  Date        `json:"guestCheckout"`                  // Reservation Check-out date
-		RoomTypeID                     string      `json:"roomTypeID"`                     // ID of the room type
-		RoomTypeName                   string      `json:"roomTypeName"`                   // Name of the room type
-		RoomName                       string      `json:"roomName"`                       // Name of the specific room. N/A means not applicable, and it is used if the transaction is not linked to a room.
-		GuestName                      string      `json:"guestName"`                      // Name of the first guest of the reservation
-		Description                    string      `json:"description"`                    // Description of the transaction
-		Category                       string      `json:"category"`                       // Category of the transaction
-		TransactionCode                string      `json:"transactionCode"`                // Transaction identifier that can be used, or left blank
-		Notes                          string      `json:"notes"`                          // If any special information needs to be added to the transaction, it will be in this field
-		Quantity                       Int         `json:"quantity"`                       // Consolidated amount on the transaction (Credit - Debit)
-		Currency                       string      `json:"currency"`                       // Currency of the transaction
-		Username                       string      `json:"userName"`                       // User responsible for creating the transaction
-		TransactionType                string      `json:"transactionType"`                // Consolidated transaction type. Toegestane waarden: debit, credit
+		PropertyID                     Int         `json:"propertyID"`
+		ReservationID                  string      `json:"reservationID"`
+		SubReservationID               string      `json:"subReservationID"`
+		HouseAccountID                 interface{} `json:"houseAccountID"`
+		HouseAccountName               string      `json:"houseAccountName"`
+		GuestID                        string      `json:"guestID"`
+		PropertyName                   string      `json:"propertyName"`
+		TransactionDateTime            DateTime    `json:"transactionDateTime"`
+		TransactionDateTimeUTC         DateTime    `json:"transactionDateTimeUTC"`
+		TransactionModifiedDateTime    DateTime    `json:"transactionModifiedDateTime"`
+		TransactionModifiedDateTimeUTC DateTime    `json:"transactionModifiedDateTimeUTC"`
+		GuestCheckin                   Date        `json:"guestCheckin"`
+		GuestCheckout                  Date        `json:"guestCheckout"`
+		RoomTypeID                     string      `json:"roomTypeID"`
+		RoomTypeName                   string      `json:"roomTypeName"`
+		RoomName                       string      `json:"roomName"`
+		GuestName                      string      `json:"guestName"`
+		Description                    string      `json:"description"`
+		Category                       string      `json:"category"`
+		TransactionCode                string      `json:"transactionCode"`
+		Notes                          string      `json:"notes"`
+		Quantity                       Int         `json:"quantity"`
+		Currency                       string      `json:"currency"`
+		Username                       string      `json:"userName"`
+		TransactionType                string      `json:"transactionType"`
 		TransactionCategory            string      `json:"transactionCategory"`
-		ItemCategoryName               string      `json:"itemCategoryName"` // Transaction category. Toegestane waarden: adjustment, addon, custom_item, fee, payment, product, rate, room_revenue, refund, tax, void
-		TransactionID                  string      `json:"transactionID"`    // Transaction identifier
-		// Parent transaction identifier. Parent transaction is a transaction to which
-		// this current transaction is strongly related to or derived from.
-		// Example: Parent transaction to a room rate tax is a room rate.
-		// This parent transaction ID will mostly be present on transactions that are
-		// taxes, fees and voids. It will not be present on room rates, items, payments
-		// and refunds.
-		ParentTransactionID string  `json:"parentTransactionID"`
-		CardType            string  `json:"cardType"` // Abbreviated name of credit card type
-		IsDeleted           bool    `json:"isDeleted"`
-		Amount              float64 `json:"amount"`
+		ItemCategoryName               string      `json:"itemCategoryName"`
+		TransactionID                  string      `json:"transactionID"`
+		ParentTransactionID            string      `json:"parentTransactionID"`
+		CardType                       string      `json:"cardType"`
+		IsDeleted                      bool        `json:"isDeleted"`
+		Amount                         float64     `json:"amount"`
 	} `json:"data"`
 }
 
